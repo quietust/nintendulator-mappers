3bfd2790c55d616fdfa84801f51010de7e108c31
Quietust <quietust@gmail.com>
2006-02-17 23:03:15 +0000

    Add CRC check for MMC5 games

--- src/iNES/mapper005.c
+++ src/iNES/mapper005.c
@@ -7,6 +7,86 @@ static	struct
 	HWND ConfigWindow;
 }	Mapper;
 
+static	unsigned long CRCtable[256];
+static	void	InitCRC (void)
+{
+	const unsigned long poly = 0xEDB88320;
+	unsigned long n;
+	int i, j;
+	for (i = 0; i < 256; i++)
+	{
+		n = i;
+		for (j = 0; j < 8; j++)
+		{
+			if (n & 1)
+				n = (n >> 1) ^ poly;
+			else	n >>= 1;
+		}
+		CRCtable[i] = n;
+	}
+}
+static	unsigned long	CalcCRC (unsigned char *data, int len)
+{
+	unsigned long CRC = 0xFFFFFFFF;
+	int i;
+	for (i = 0; i < len; i++)
+		CRC = (CRC >> 8) ^ CRCtable[(CRC ^ data[i]) & 0xFF];
+	return CRC ^ 0xFFFFFFFF;
+}
+static	struct
+{
+	unsigned long CRC;
+	int SRAMconf;
+} MMC5Carts[23] = {
+	{0x95CA9EC7,MMC5WRAM_0KB_0KB},	// Castlevania III - Dracula's Curse (U)
+	{0x51D2112F,MMC5WRAM_0KB_0KB},	// Laser Invasion (U)
+	{0x255B129C,MMC5WRAM_0KB_0KB},	// (?) Gun Sight (J)
+
+	{0xE7C72DBB,MMC5WRAM_8KB_0KB},	// Gemfire (U)
+	{0x57F33F70,MMC5WRAM_8KB_0KB},	// (?) Royal Blood (J)
+	{0x5D9D9891,MMC5WRAM_8KB_0KB},	// (?) Just Breed (J)
+
+	{0x2B548D75,MMC5WRAM_8KB_8KB},	// Bandit Kings of Ancient China (U)
+	{0xE6C28C5F,MMC5WRAM_8KB_8KB},	// (?) Suikoden - Tenmei no Chikai (J)
+	{0x2F50BD38,MMC5WRAM_8KB_8KB},	// L'Empereur (U)
+	{0x8E9A5E2F,MMC5WRAM_8KB_8KB},	// L'Empereur (U) [a1]
+	{0x57E3218B,MMC5WRAM_8KB_8KB},	// (?) L'Empereur (J)
+	{0xB56958D1,MMC5WRAM_8KB_8KB},	// Nobunaga's Ambition 2 (U)
+	{0x98C8E090,MMC5WRAM_8KB_8KB},	// (?) Nobunaga no Yabou - Sengoku Gunyuu Den (J)
+	{0xCD35E2E9,MMC5WRAM_8KB_8KB},	// Uncharted Waters (U)
+	{0xF4CD4998,MMC5WRAM_8KB_8KB},	// (?) Daikoukai Jidai (J)
+
+	{0x11EAAD26,MMC5WRAM_32KB_0KB},	// Romance of the Three Kingdoms II (U)
+	{0x95BA5733,MMC5WRAM_32KB_0KB},	// (?) Sangokushi 2 (J)
+
+	{0xF4120E58,MMC5WRAM_MAXOPTS},	// Aoki Ookami to Shiroki Mejika - Genchou Hishi (J)
+	{0x8FA95456,MMC5WRAM_MAXOPTS},	// Ishin no Arashi (J)
+	{0xCD9ACF43,MMC5WRAM_MAXOPTS},	// Metal Slader Glory (J)
+	{0x286613D8,MMC5WRAM_MAXOPTS},	// Nobunaga no Yabou - Bushou Fuuun Roku (J)
+	{0xE91548D8,MMC5WRAM_MAXOPTS},	// Shin 4 Nin Uchi Mahjong - Yakuman Tengoku (J)
+	{0xD979C8B7,MMC5WRAM_MAXOPTS}	// Uchuu Keibitai SDF (J)
+};
+
+static	int	CheckSRAM (void)
+{
+	int orig, i, size = MMC5WRAM_MAXOPTS;
+	unsigned long CRC;
+	if (!(orig = EMU->GetPRG_ROM4(8)))
+		orig = -1;
+
+	EMU->SetPRG_ROM4(8,0);
+	CRC = CalcCRC(EMU->GetPRG_Ptr4(8),ROM->INES_PRGSize << 14);
+	for (i = 0; i < 23; i++)
+		if (MMC5Carts[i].CRC == CRC)
+			size = MMC5Carts[i].SRAMconf;
+
+	if (orig == -1)
+		EMU->SetPRG_OB4(8);
+	else	EMU->SetPRG_ROM4(8,orig);
+	return size;
+}
+
+static	void	SetSRAM (void);
 static	LRESULT CALLBACK ConfigProc(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
 {
 	switch (message)
@@ -25,21 +105,16 @@ static	LRESULT CALLBACK ConfigProc(HWND hDlg, UINT message, WPARAM wParam, LPARA
 			case MMC5WRAM_32KB_32KB:CheckRadioButton(hDlg,IDC_MAPPER5_0_0,IDC_MAPPER5_32_32,IDC_MAPPER5_32_32);	break;
 			}
 			SetDlgItemText(hDlg,IDC_MAPPER5_DESCRIBE,
-				"Bandit Kings of Ancient China // Suikoden - Tenmei no Chikai\n"
+				"The following Famicom games have unknown SRAM sizes:\n"
+				"Aoki Ookami to Shiroki Mejika - Genchou Hishi\n"
+				"Ishin no Arashi\n"
+				"Metal Slader Glory\n"
+				"Nobunaga no Yabou - Bushou Fuuun Roku\n"
 				"Shin 4 Nin Uchi Mahjong - Yakuman Tengoku\n"
 				"Uchuu Keibitai SDF\n"
-				"Sangokushi 2 // Romance of the Three Kingdoms II\n"
-				"Royal Blood // Gemfire\n"
-				"Nobunaga no Yabou - Sengoku Gunyuu Den // Nobunaga's Ambition 2\n"
-				"Nobunaga no Yabou - Bushou Fuuun Roku // ???\n"
-				"Metal Slader Glory (norm/alt)\n"
-				"Gun Sight (norm/alt) // Laser Invasion\n"
-				"L'Empereur // L'Empereur (norm/alt)\n"
-				"Just Breed\n"
-				"Ishin no Arashi // ???\n"
-				"Daokoukai Jidai // Uncharted Waters\n"
-				"Aoki Ookami to Shiroki Mejika - Genchou Hishi // ???\n"
-				"Castlevania 3\n"
+				"\n"
+				"If you have any of these games and can open the cartridge without damaging it,\n"
+				"please email the author with the game's PCB label (should match 'HVC-E_ROM')."
 				);
 			return FALSE;
 		case WM_COMMAND:
@@ -47,51 +122,24 @@ static	LRESULT CALLBACK ConfigProc(HWND hDlg, UINT message, WPARAM wParam, LPARA
 			{
 			case IDOK:
 				if (IsDlgButtonChecked(hDlg,IDC_MAPPER5_0_0) == BST_CHECKED)
-				{
 					MMC5.WRAMsize = MMC5WRAM_0KB_0KB;
-					EMU->Set_SRAMSize(0);
-				}
 				else if (IsDlgButtonChecked(hDlg,IDC_MAPPER5_0_8) == BST_CHECKED)
-				{
 					MMC5.WRAMsize = MMC5WRAM_0KB_8KB;
-					EMU->Set_SRAMSize(0);
-				}
 				else if (IsDlgButtonChecked(hDlg,IDC_MAPPER5_0_32) == BST_CHECKED)
-				{
 					MMC5.WRAMsize = MMC5WRAM_0KB_32KB;
-					EMU->Set_SRAMSize(0);
-				}
 				else if (IsDlgButtonChecked(hDlg,IDC_MAPPER5_8_0) == BST_CHECKED)
-				{
 					MMC5.WRAMsize = MMC5WRAM_8KB_0KB;
-					EMU->Set_SRAMSize(8);
-				}
 				else if (IsDlgButtonChecked(hDlg,IDC_MAPPER5_8_8) == BST_CHECKED)
-				{
 					MMC5.WRAMsize = MMC5WRAM_8KB_8KB;
-					EMU->Set_SRAMSize(8);
-				}
 				else if (IsDlgButtonChecked(hDlg,IDC_MAPPER5_8_32) == BST_CHECKED)
-				{
 					MMC5.WRAMsize = MMC5WRAM_8KB_32KB;
-					EMU->Set_SRAMSize(8);
-				}
 				else if (IsDlgButtonChecked(hDlg,IDC_MAPPER5_32_0) == BST_CHECKED)
-				{
 					MMC5.WRAMsize = MMC5WRAM_32KB_0KB;
-					EMU->Set_SRAMSize(32);
-				}
 				else if (IsDlgButtonChecked(hDlg,IDC_MAPPER5_32_8) == BST_CHECKED)
-				{
 					MMC5.WRAMsize = MMC5WRAM_32KB_8KB;
-					EMU->Set_SRAMSize(32);
-				}
 				else if (IsDlgButtonChecked(hDlg,IDC_MAPPER5_32_32) == BST_CHECKED)
-				{
 					MMC5.WRAMsize = MMC5WRAM_32KB_32KB;
-					EMU->Set_SRAMSize(32);
-				}
-				EMU->Load_SRAM();
+				SetSRAM();
 				MMC5_SyncPRG();	
 				MessageBox(hWnd,"Please perform a SOFT reset to ensure proper functionality!","INES.DLL",MB_OK);
 			case IDCANCEL:
@@ -115,6 +163,35 @@ static	void	_MAPINT	Config (void)
 		SetWindowPos(Mapper.ConfigWindow,hWnd,0,0,0,0,SWP_SHOWWINDOW | SWP_NOSIZE);
 }
 
+static	void	SetSRAM (void)
+{
+	switch (MMC5.WRAMsize)
+	{
+	case MMC5WRAM_0KB_0KB:
+	case MMC5WRAM_0KB_8KB:
+	case MMC5WRAM_0KB_32KB:
+		break;
+
+	case MMC5WRAM_8KB_0KB:
+	case MMC5WRAM_8KB_8KB:
+	case MMC5WRAM_8KB_32KB:
+		EMU->Set_SRAMSize(8192);
+		break;
+
+	case MMC5WRAM_32KB_0KB:
+	case MMC5WRAM_32KB_8KB:
+	case MMC5WRAM_32KB_32KB:
+		EMU->Set_SRAMSize(32768);
+		break;
+
+	case MMC5WRAM_MAXOPTS:
+		Config();
+		break;
+	}
+	if (ROM->INES_Flags & 0x02)
+		EMU->Load_SRAM();
+}
+
 static	void	_MAPINT	Shutdown (void)
 {
 	iNES_UnloadROM();
@@ -129,9 +206,10 @@ static	void	_MAPINT	Reset (int IsHardReset)
 	iNES_InitROM();
 
 	if (IsHardReset)
-		MMC5.WRAMsize = MMC5WRAM_8KB_8KB;
+		MMC5.WRAMsize = CheckSRAM();
 
 	Mapper.ConfigWindow = NULL;
+	SetSRAM();
 
 	MMC5_Init();
 }
