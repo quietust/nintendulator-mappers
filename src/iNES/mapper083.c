cefecffae8e4dc3d20ab0de18db086079bd66a0b
Quietust <quietust@gmail.com>
2006-02-17 23:10:05 +0000

    Improve mapper 83 (Cony)

--- src/iNES/mapper083.c
+++ src/iNES/mapper083.c
@@ -2,25 +2,60 @@
 
 static	struct
 {
-	u8 Mirror;
-	u8 PRG[4];
+	u8 Mode;
+	u8 PRG[5];
 	u8 CHR[8];
-	u8 Bank;
 	u16_n IRQcounter;
-	u8 IRQenabled;
 }	Mapper;
 
 static	void	Sync (void)
 {
 	u8 x;
-	for (x = 0; x < 4; x++)
-		EMU->SetPRG_ROM8(8 | (x << 1),Mapper.PRG[x]);
-	if (ROM->INES_CHRSize >= 32)
-		for (x = 0; x < 4; x++)
-			EMU->SetCHR_ROM2(x << 1,Mapper.CHR[(x & 2) ? (x | 6) : (x)]);
-	else	for (x = 0; x < 8; x++)
+	if (Mapper.Mode & 0x08)
+	{
+		if (Mapper.Mode & 0x10)
+			EMU->SetPRG_ROM8(0x6,Mapper.PRG[3]);
+		else
+		{
+			EMU->SetPRG_OB4(0x6);
+			EMU->SetPRG_OB4(0x7);
+		}
+		EMU->SetPRG_ROM8(0x8,Mapper.PRG[0]);
+		EMU->SetPRG_ROM8(0xA,Mapper.PRG[1]);
+		EMU->SetPRG_ROM8(0xC,Mapper.PRG[2]);
+		EMU->SetPRG_ROM8(0xE,0x1F);
+	}
+	else
+	{
+		if (Mapper.Mode & 0x10)
+			EMU->SetPRG_ROM8(0x6,0x1F);
+		else
+		{
+			EMU->SetPRG_OB4(0x6);
+			EMU->SetPRG_OB4(0x7);
+		}
+		if (Mapper.Mode & 0x4)
+			EMU->SetPRG_ROM16(0x8,Mapper.PRG[4]);
+		else
+		{
+			EMU->SetPRG_ROM16(0x8,Mapper.PRG[4]);
+			EMU->SetPRG_ROM16(0xC,0xF);
+		}
+
+	}
+	if (ROM->INES_CHRSize > 32)
+	{
+		EMU->SetCHR_ROM2(0,Mapper.CHR[0]);
+		EMU->SetCHR_ROM2(2,Mapper.CHR[1]);
+		EMU->SetCHR_ROM2(4,Mapper.CHR[6]);
+		EMU->SetCHR_ROM2(6,Mapper.CHR[7]);
+	}
+	else
+	{
+		for (x = 0; x < 8; x++)
 			EMU->SetCHR_ROM1(x,Mapper.CHR[x]);
-	switch (Mapper.Mirror & 3)
+	}
+	switch (Mapper.Mode & 3)
 	{
 	case 0:	EMU->Mirror_V();	break;
 	case 1:	EMU->Mirror_H();	break;
@@ -32,14 +67,12 @@ static	void	Sync (void)
 static	int	_MAPINT	SaveLoad (STATE_TYPE mode, int x, unsigned char *data)
 {
 	u8 i;
-	SAVELOAD_BYTE(mode,x,data,Mapper.Mirror)
-	for (i = 0; i < 4; i++)
+	SAVELOAD_BYTE(mode,x,data,Mapper.Mode)
+	for (i = 0; i < 5; i++)
 		SAVELOAD_BYTE(mode,x,data,Mapper.PRG[i])
 	for (i = 0; i < 8; i++)
 		SAVELOAD_BYTE(mode,x,data,Mapper.CHR[i])
-	SAVELOAD_BYTE(mode,x,data,Mapper.Bank)
 	SAVELOAD_WORD(mode,x,data,Mapper.IRQcounter.s0)
-	SAVELOAD_BYTE(mode,x,data,Mapper.IRQenabled)
 	if (mode == STATE_LOAD)
 		Sync();
 	return x;
@@ -47,40 +80,36 @@ static	int	_MAPINT	SaveLoad (STATE_TYPE mode, int x, unsigned char *data)
 
 static	void	_MAPINT	CPUCycle (void)
 {
-	if (Mapper.IRQenabled)
+	if (Mapper.Mode & 0x80)
 	{
 		Mapper.IRQcounter.s0--;
 		if (!Mapper.IRQcounter.s0)
-		{
 			EMU->SetIRQ(0);
-			Mapper.IRQenabled = 0;
-		}
 	}
 }
 
 static	void	_MAPINT	Write (int Bank, int Addr, int Val)
 {
-	switch (Addr & 0x317)
+	switch ((Addr >> 8) & 0x3)
 	{
-	case 0x000:	Mapper.PRG[0] = ((Val << 1) & 0x1E) | 0x0;
-			Mapper.PRG[1] = ((Val << 1) & 0x1E) | 0x1;	break;
-	case 0x100:	Mapper.Mirror = Val;		break;
-	case 0x200:	Mapper.IRQcounter.b0 = Val;
-			EMU->SetIRQ(1);			break;
-	case 0x201:	Mapper.IRQcounter.b1 = Val;
-			Mapper.IRQenabled = 1;		break;
-	case 0x300:	Mapper.PRG[0] = Val & 0x1F;	break;
-	case 0x301:	Mapper.PRG[1] = Val & 0x1F;	break;
-	case 0x302:	Mapper.PRG[2] = Val & 0x1F;	break;
-//	case 0x303:	Mapper.PRG[3] = Val & 0x1F;	break;
-	case 0x310:	Mapper.CHR[0] = Val;	break;
-	case 0x311:	Mapper.CHR[1] = Val;	break;
-	case 0x312:	Mapper.CHR[2] = Val;	break;
-	case 0x313:	Mapper.CHR[3] = Val;	break;
-	case 0x314:	Mapper.CHR[4] = Val;	break;
-	case 0x315:	Mapper.CHR[5] = Val;	break;
-	case 0x316:	Mapper.CHR[6] = Val;	break;
-	case 0x317:	Mapper.CHR[7] = Val;	break;
+	case 0:	Mapper.PRG[4] = Val;
+		break;
+	case 1:	Mapper.Mode = Val;
+		break;
+	case 2:	if (Addr & 1)
+			Mapper.IRQcounter.b1 = Val;
+		else
+		{
+			Mapper.IRQcounter.b0 = Val;
+			EMU->SetIRQ(1);
+		}
+		break;
+	case 3:	if (Addr & 0x10)
+		{
+			if (!(Addr & 0x8))
+				Mapper.CHR[Addr & 0x7] = Val;
+		}
+		else	Mapper.PRG[Addr & 0x3] = Val & 0x1F;
 	}
 	Sync();
 }
@@ -93,13 +122,12 @@ static	void	_MAPINT	Reset (RESET_TYPE ResetType)
 
 	if (ResetType == RESET_HARD)
 	{
-		Mapper.Mirror = 0;
+		Mapper.Mode = 0;
 		for (x = 0; x < 8; x++)
 			Mapper.CHR[x] = 0;
-		Mapper.PRG[0] = 0x00;
-		Mapper.PRG[1] = 0x01;
-		Mapper.PRG[2] = 0x1E;
-		Mapper.PRG[3] = 0x1F;
+		for (x = 0; x < 5; x++)
+			Mapper.PRG[x] = 0;
+		Mapper.IRQcounter.s0 = 0;
 	}
 
 	Sync();
@@ -110,7 +138,7 @@ CTMapperInfo	MapperInfo_083 =
 {
 	&MapperNum,
 	"Cony",
-	COMPAT_PARTIAL,
+	COMPAT_NEARLY,
 	NULL,
 	Reset,
 	NULL,
