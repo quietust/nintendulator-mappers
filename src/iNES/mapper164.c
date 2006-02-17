9c8b1b5a0958134c1147a89585e71c284d61c13c
Quietust <quietust@gmail.com>
2006-02-17 23:10:19 +0000

    Simplify mapper 164

--- src/iNES/mapper164.c
+++ src/iNES/mapper164.c
@@ -2,14 +2,13 @@
 
 static	struct
 {
-	FCPUWrite Write4;
 	u8 Reg;
 }	Mapper;
 
 static	void	Sync (void)
 {
-	EMU->SetCHR_RAM8(0,0);
 	EMU->SetPRG_ROM32(0x8,Mapper.Reg);
+	EMU->SetCHR_RAM8(0,0);
 }
 
 static	int	_MAPINT	SaveLoad (STATE_TYPE mode, int x, unsigned char *data)
@@ -22,11 +21,11 @@ static	int	_MAPINT	SaveLoad (STATE_TYPE mode, int x, unsigned char *data)
 
 static	void	_MAPINT	Write (int Bank, int Addr, int Val)
 {
-	int addr = (Bank << 12) | Addr;
-	if (Bank == 4) Mapper.Write4(Bank,Addr,Val);
-	if ((addr & 0x7300) == 0x5000)
+	if (!(Addr & 0x300))
+	{
 		Mapper.Reg = Val;
-	Sync();
+		Sync();
+	}
 }
 
 static	void	_MAPINT	Load (void)
@@ -35,15 +34,11 @@ static	void	_MAPINT	Load (void)
 }
 static	void	_MAPINT	Reset (RESET_TYPE ResetType)
 {
-	u8 x;
 	iNES_SetMirroring();
 
-	Mapper.Write4 = EMU->GetCPUWriteHandler(0x4);
 	EMU->SetPRG_RAM8(0x6,0);
-	for (x = 4; x < 6; x++)
-		EMU->SetCPUWriteHandler(x,Write);
-	for (x = 8; x <= 0xF; x++)
-		EMU->SetCPUWriteHandler(x,Write);
+	EMU->SetCPUWriteHandler(0x5,Write);
+	EMU->SetCPUWriteHandler(0xD,Write);
 
 	if (ResetType == RESET_HARD)
 		Mapper.Reg = 0xFF;
