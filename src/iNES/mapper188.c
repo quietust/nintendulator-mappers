eced06f556b0406206300ffd70932678bd8c28d0
Quietust <quietust@gmail.com>
2006-02-17 23:10:38 +0000

    (no comment)

---
+++ src/iNES/mapper188.c
@@ -0,0 +1,41 @@
+#include	"..\DLL\d_iNES.h"
+#include	"..\Hardware\h_Latch.h"
+
+static	void	Sync (void)
+{
+	EMU->SetPRG_ROM16(0x8,0);
+	EMU->SetPRG_ROM16(0xC,Latch.Data & 0x7);
+	if (ROM->INES_CHRSize)
+		EMU->SetCHR_ROM8(0,0);
+	else	EMU->SetCHR_RAM8(0,0);
+}
+
+static	void	_MAPINT	Load (void)
+{
+	Latch_Load(Sync,FALSE);
+}
+static	void	_MAPINT	Reset (RESET_TYPE ResetType)
+{
+	iNES_SetMirroring();
+	Latch_Reset(ResetType);
+}
+static	void	_MAPINT	Unload (void)
+{
+	Latch_Unload();
+}
+
+static	u8 MapperNum = 180;
+CTMapperInfo	MapperInfo_180 =
+{
+	&MapperNum,
+	"Mapper 180",
+	COMPAT_FULL,
+	Load,
+	Reset,
+	Unload,
+	NULL,
+	NULL,
+	Latch_SaveLoad_D,
+	NULL,
+	NULL
+};
