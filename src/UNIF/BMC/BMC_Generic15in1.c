1406f118d21c1f7529fcfcdec22fe058a832bab5
Quietust <quietust@gmail.com>
2006-02-17 23:05:06 +0000

    Fix mapper

--- src/UNIF/BMC/BMC_Generic15in1.c
+++ src/UNIF/BMC/BMC_Generic15in1.c
@@ -1,27 +1,47 @@
 #include	"..\..\DLL\d_UNIF.h"
-#include	"..\..\Hardware\h_Latch.h"
+
+struct
+{
+	u8 Data;
+}	Mapper;
 
 static	void	Sync (void)
 {
-	u8 What = EMU->GetCPUReadHandler(Latch.Addr.s0 >> 12)(Latch.Addr.n3,Latch.Addr.s0 & 0xFFF);
-	if (What & 0x0E)
+	if (Mapper.Data & 0x0E)
 	{
 		EMU->SetPRG_ROM16(0x8,0x0F);
-		EMU->SetPRG_ROM16(0xC,What & 0x0F);
+		EMU->SetPRG_ROM16(0xC,Mapper.Data & 0x0F);
 	}
 	else	EMU->SetPRG_ROM32(0x8,0);
-	EMU->SetCHR_ROM8(0,What & 0x0F);
+	EMU->SetCHR_ROM8(0,Mapper.Data & 0x0F);
+}
+
+int	_MAPINT	SaveLoad (int mode, int x, char *data)
+{
+	SAVELOAD_BYTE(mode,x,data,Mapper.Data)
+	if (mode == STATE_LOAD)
+		Sync();
+	return x;
+}
+
+static	void	_MAPINT	Write (int Bank, int Where, int What)
+{
+	Mapper.Data = EMU->GetCPUReadHandler(Bank)(Bank,Where);
+	Sync();
 }
 
 static	void	_MAPINT	Shutdown (void)
 {
-	Latch_Destroy();
 }
 
 static	void	_MAPINT	Reset (int IsHardReset)
 {
-	Latch_Init(Sync,IsHardReset,FALSE);
+	u8 x;
+	for (x = 0x8; x <= 0xF; x++)
+		EMU->SetCPUWriteHandler(x,Write);
+	Mapper.Data = 0;
 	UNIF_SetMirroring(NULL);
+	Sync();
 }
 
 CTMapperInfo	MapperInfo_BMC_Generic15in1 =
@@ -33,7 +53,7 @@ CTMapperInfo	MapperInfo_BMC_Generic15in1 =
 	Shutdown,
 	NULL,
 	NULL,
-	Latch_SaveLoad_A,
+	SaveLoad,
 	NULL,
 	NULL
 };
