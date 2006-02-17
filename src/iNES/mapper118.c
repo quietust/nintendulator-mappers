f60be4a69a82393718634f1a8bc5b84ef3420314
Quietust <quietust@gmail.com>
2006-02-17 23:03:10 +0000

    Simplify TLSROM and mapper 95

--- src/iNES/mapper118.c
+++ src/iNES/mapper118.c
@@ -3,7 +3,6 @@
 
 static	struct
 {
-	u8 Cmd;
 	u8 LastMirror;
 }	Mapper;
 
@@ -19,14 +18,18 @@ static	void	_MAPINT	PPUCycle (int Addr, int Scanline, int Cycle, int IsRendering
 	if (MMC3_GetCHRBank((Addr >> 10) & 0x7) & 0x80)
 	{
 		if (Mapper.LastMirror == 0)
+		{
 			EMU->Mirror_S1();
-		Mapper.LastMirror = 1;
+			Mapper.LastMirror = 1;
+		}
 	}
 	else
 	{
 		if (Mapper.LastMirror == 1)
+		{
 			EMU->Mirror_S0();
-		Mapper.LastMirror = 0;
+			Mapper.LastMirror = 0;
+		}
 	}
 	MMC3_PPUCycle(Addr,Scanline,Cycle,IsRendering);
 }
@@ -37,30 +40,6 @@ static	void	_MAPINT	Shutdown (void)
 	MMC3_Destroy();
 }
 
-static	void	_MAPINT	Write (int Bank, int Where, int What)
-{
-	MMC3_CPUWrite89(Bank,Where,What);
-	if (Where & 1)
-	{
-		if (Mapper.Cmd < 6)
-		{
-			if (What & 0x80)
-			{
-				if (Mapper.LastMirror == 0)
-					EMU->Mirror_S1();
-				Mapper.LastMirror = 1;
-			}
-			else
-			{
-				if (Mapper.LastMirror == 1)
-					EMU->Mirror_S0();
-				Mapper.LastMirror = 0;
-			}
-		}
-	}
-	else	Mapper.Cmd = What & 7;
-}
-
 static	void	_MAPINT	Reset (int IsHardReset)
 {
 	iNES_InitROM();
@@ -69,9 +48,6 @@ static	void	_MAPINT	Reset (int IsHardReset)
 	EMU->Mirror_S0();
 
 	MMC3_Init(Sync);
-
-	EMU->SetCPUWriteHandler(0x8,Write);
-	EMU->SetCPUWriteHandler(0x9,Write);
 }
 
 static	u8 MapperNum = 118;
@@ -79,7 +55,7 @@ CTMapperInfo	MapperInfo_118 =
 {
 	&MapperNum,
 	"TKSROM/TLSROM (MMC3)",
-	COMPAT_PARTIAL,
+	COMPAT_FULL,
 	Reset,
 	Shutdown,
 	NULL,
