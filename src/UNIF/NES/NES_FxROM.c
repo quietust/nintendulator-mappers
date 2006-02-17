6d990a411a9b7e023c1f84a0508275d5419b16cb
Quietust <quietust@gmail.com>
2006-02-17 23:04:01 +0000

    Add MMC4

--- src/UNIF/NES/NES_FxROM.c
+++ src/UNIF/NES/NES_FxROM.c
@@ -1,34 +1,36 @@
 #include "..\..\DLL\d_UNIF.h"
-#include "..\..\Hardware\h_MMC2.h"
+#include "..\..\Hardware\h_MMC4.h"
 
-static	void	Sync_PNROM (void)
+static	void	Sync_FJROM (void)
 {
-	MMC2_SyncPRG(0xF,0);
-	MMC2_SyncCHR();
-	MMC2_SyncMirror();
+	MMC4_SyncPRG(0xF,0);
+	MMC4_SyncCHR();
+	MMC4_SyncMirror();
+	EMU->SetPRG_RAM8(0x6,0);
 }
 
 static	void	_MAPINT	Shutdown (void)
 {
-	MMC2_Destroy();
+	UNIF_SaveSRAM();
+	MMC4_Destroy();
 }
 
-static	void	_MAPINT	Reset_PNROM (int IsHardReset)
+static	void	_MAPINT	Reset_FJROM (int IsHardReset)
 {
-	MMC2_Init(Sync_PNROM);
-	UNIF_SetMirroring(NULL);
+	MMC4_Init(Sync_FJROM);
+	UNIF_InitSRAM(8192);
 }
 
-CTMapperInfo	MapperInfo_NES_PNROM =
+CTMapperInfo	MapperInfo_NES_FJROM =
 {
-	"NES-PNROM",
-	"MMC2",
+	"NES-FJROM",
+	"MMC4",
 	COMPAT_FULL,
-	Reset_PNROM,
+	Reset_FJROM,
 	Shutdown,
 	NULL,
 	NULL,
-	MMC2_SaveLoad,
+	MMC4_SaveLoad,
 	NULL,
 	NULL
 };
