3364f5c4b344fcc5258495db1f8549c95c4c1aab
Quietust <quietust@gmail.com>
2006-02-17 23:12:32 +0000

    Re-enable coin buttons properly, and set initial dialog state appropriately

--- src/Hardware/h_VS.c
+++ src/Hardware/h_VS.c
@@ -52,7 +52,10 @@ int	_MAPINT	VS_Read (int Bank, int Addr)
 void	_MAPINT	VS_CPUCycle (void)
 {
 	if ((VS.CoinDelay) && (!--VS.CoinDelay))
+	{
 		VS.Coin = 0;
+		if (
+	}
 }
 
 static	void	BlockDialog (HWND hDlg, int dlgItem)
@@ -86,152 +89,171 @@ static	void	UnblockDialog (HWND hDlg)
 		EnableWindow(GetDlgItem(hDlg,IDC_VS_COIN2),TRUE);
 		EnableWindow(GetDlgItem(hDlg,IDC_VS_COIN3),TRUE);
 	}
+	else
+	{
+		EnableWindow(GetDlgItem(hDlg,IDC_VS_COIN1),FALSE);
+		EnableWindow(GetDlgItem(hDlg,IDC_VS_COIN2),FALSE);
+		EnableWindow(GetDlgItem(hDlg,IDC_VS_COIN3),FALSE);
+	}
 }
 
 static	LRESULT CALLBACK ConfigProc(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
 {
+	const int buttons[8] = {IDC_VS_DIP0,IDC_VS_DIP1,IDC_VS_DIP2,IDC_VS_DIP3,IDC_VS_DIP4,IDC_VS_DIP5,IDC_VS_DIP6,IDC_VS_DIP7};
+	int i;
 	switch (message)
 	{
-		case WM_INITDIALOG:
-			return FALSE;
-		case WM_COMMAND:
-			switch (LOWORD(wParam))
-			{
-			case IDC_VS_DIP0:
-				if (VS.ConfigCmd == 0)
-				{	// if there was no pending command, then disable all other dialog controls
-					// so as to avoid command overlaps
-					VS.ConfigCmd = 1;
-					if (IsDlgButtonChecked(hDlg,IDC_VS_DIP0) == BST_UNCHECKED)
-						VS.ConfigCmd += 8;
-					BlockDialog(hDlg,IDC_VS_DIP0);
-				}
-				else	// if there WAS a pending command, it was to change the state of this DIP
-				{	// so we can abort it and re-enable the others
-					UnblockDialog(hDlg);
-					VS.ConfigCmd = 0;
-				}
-				return TRUE;		break;
-			case IDC_VS_DIP1:
-				if (VS.ConfigCmd == 0)
-				{
-					VS.ConfigCmd = 2;
-					if (IsDlgButtonChecked(hDlg,IDC_VS_DIP1) == BST_UNCHECKED)
-						VS.ConfigCmd += 8;
-					BlockDialog(hDlg,IDC_VS_DIP1);
-				}
-				else
-				{
-					UnblockDialog(hDlg);
-					VS.ConfigCmd = 0;
-				}
-				return TRUE;		break;
-			case IDC_VS_DIP2:
-				if (VS.ConfigCmd == 0)
-				{
-					VS.ConfigCmd = 3;
-					if (IsDlgButtonChecked(hDlg,IDC_VS_DIP2) == BST_UNCHECKED)
-						VS.ConfigCmd += 8;
-					BlockDialog(hDlg,IDC_VS_DIP2);
-				}
-				else
-				{
-					UnblockDialog(hDlg);
-					VS.ConfigCmd = 0;
-				}
-				return TRUE;		break;
-			case IDC_VS_DIP3:
-				if (VS.ConfigCmd == 0)
-				{
-					VS.ConfigCmd = 4;
-					if (IsDlgButtonChecked(hDlg,IDC_VS_DIP3) == BST_UNCHECKED)
-						VS.ConfigCmd += 8;
-					BlockDialog(hDlg,IDC_VS_DIP3);
-				}
-				else
-				{
-					UnblockDialog(hDlg);
-					VS.ConfigCmd = 0;
-				}
-				return TRUE;		break;
-			case IDC_VS_DIP4:
-				if (VS.ConfigCmd == 0)
-				{
-					VS.ConfigCmd = 5;
-					if (IsDlgButtonChecked(hDlg,IDC_VS_DIP4) == BST_UNCHECKED)
-						VS.ConfigCmd += 8;
-					BlockDialog(hDlg,IDC_VS_DIP4);
-				}
-				else
-				{
-					UnblockDialog(hDlg);
-					VS.ConfigCmd = 0;
-				}
-				return TRUE;		break;
-			case IDC_VS_DIP5:
-				if (VS.ConfigCmd == 0)
-				{
-					VS.ConfigCmd = 6;
-					if (IsDlgButtonChecked(hDlg,IDC_VS_DIP5) == BST_UNCHECKED)
-						VS.ConfigCmd += 8;
-					BlockDialog(hDlg,IDC_VS_DIP5);
-				}
-				else
-				{
-					UnblockDialog(hDlg);
-					VS.ConfigCmd = 0;
-				}
-				return TRUE;		break;
-			case IDC_VS_DIP6:
-				if (VS.ConfigCmd == 0)
-				{
-					VS.ConfigCmd = 7;
-					if (IsDlgButtonChecked(hDlg,IDC_VS_DIP6) == BST_UNCHECKED)
-						VS.ConfigCmd += 8;
-					BlockDialog(hDlg,IDC_VS_DIP6);
-				}
-				else
-				{
-					UnblockDialog(hDlg);
-					VS.ConfigCmd = 0;
-				}
-				return TRUE;		break;
-			case IDC_VS_DIP7:
-				if (VS.ConfigCmd == 0)
-				{
-					VS.ConfigCmd = 8;
-					if (IsDlgButtonChecked(hDlg,IDC_VS_DIP7) == BST_UNCHECKED)
-						VS.ConfigCmd += 8;
-					BlockDialog(hDlg,IDC_VS_DIP7);
-				}
-				else
-				{
-					UnblockDialog(hDlg);
-					VS.ConfigCmd = 0;
-				}
-				return TRUE;		break;
-			case IDC_VS_COIN1:
-				VS.ConfigCmd = 17;
-				BlockDialog(hDlg, 0);
-				return TRUE;		break;
-			case IDC_VS_COIN2:
-				VS.ConfigCmd = 18;
-				BlockDialog(hDlg, 0);
-				return TRUE;		break;
-			case IDC_VS_COIN3:
-				VS.ConfigCmd = 19;
-				BlockDialog(hDlg, 0);
-				return TRUE;		break;
-			case IDCLOSE:
-				VS.ConfigWindow = NULL;
-				DestroyWindow(hDlg);
-				return TRUE;		break;
+	case WM_INITDIALOG:
+		if (VS.ConfigCmd)
+		{
+			if (VS.ConfigCmd < 9)
+				BlockDialog(hDlg,VS.ConfigCmd - 1);
+			else if (VS.ConfigCmd < 17)
+				BlockDialog(hDlg,VS.ConfigCmd - 9);
+			else	BlockDialog(hDlg,0);
+		}
+		else	UnblockDialog(hDlg);
+		for (i = 0; i < 8; i++)
+			CheckDlgButton(hDlg,buttons[i],(VS.DipSwitch & (1 << i)) ? BST_CHECKED : BST_UNCHECKED);
+		return FALSE;
+	case WM_COMMAND:
+		switch (LOWORD(wParam))
+		{
+		case IDC_VS_DIP0:
+			if (VS.ConfigCmd == 0)
+			{	// if there was no pending command, then disable all other dialog controls
+				// so as to avoid command overlaps
+				VS.ConfigCmd = 1;
+				if (IsDlgButtonChecked(hDlg,IDC_VS_DIP0) == BST_UNCHECKED)
+					VS.ConfigCmd += 8;
+				BlockDialog(hDlg,IDC_VS_DIP0);
 			}
-			break;
-		case WM_CLOSE:
+			else	// if there WAS a pending command, it was to change the state of this DIP
+			{	// so we can abort it and re-enable the others
+				UnblockDialog(hDlg);
+				VS.ConfigCmd = 0;
+			}
+			return TRUE;		break;
+		case IDC_VS_DIP1:
+			if (VS.ConfigCmd == 0)
+			{
+				VS.ConfigCmd = 2;
+				if (IsDlgButtonChecked(hDlg,IDC_VS_DIP1) == BST_UNCHECKED)
+					VS.ConfigCmd += 8;
+				BlockDialog(hDlg,IDC_VS_DIP1);
+			}
+			else
+			{
+				UnblockDialog(hDlg);
+				VS.ConfigCmd = 0;
+			}
+			return TRUE;		break;
+		case IDC_VS_DIP2:
+			if (VS.ConfigCmd == 0)
+			{
+				VS.ConfigCmd = 3;
+				if (IsDlgButtonChecked(hDlg,IDC_VS_DIP2) == BST_UNCHECKED)
+					VS.ConfigCmd += 8;
+				BlockDialog(hDlg,IDC_VS_DIP2);
+			}
+			else
+			{
+				UnblockDialog(hDlg);
+				VS.ConfigCmd = 0;
+			}
+			return TRUE;		break;
+		case IDC_VS_DIP3:
+			if (VS.ConfigCmd == 0)
+			{
+				VS.ConfigCmd = 4;
+				if (IsDlgButtonChecked(hDlg,IDC_VS_DIP3) == BST_UNCHECKED)
+					VS.ConfigCmd += 8;
+				BlockDialog(hDlg,IDC_VS_DIP3);
+			}
+			else
+			{
+				UnblockDialog(hDlg);
+				VS.ConfigCmd = 0;
+			}
+			return TRUE;		break;
+		case IDC_VS_DIP4:
+			if (VS.ConfigCmd == 0)
+			{
+				VS.ConfigCmd = 5;
+				if (IsDlgButtonChecked(hDlg,IDC_VS_DIP4) == BST_UNCHECKED)
+					VS.ConfigCmd += 8;
+				BlockDialog(hDlg,IDC_VS_DIP4);
+			}
+			else
+			{
+				UnblockDialog(hDlg);
+				VS.ConfigCmd = 0;
+			}
+			return TRUE;		break;
+		case IDC_VS_DIP5:
+			if (VS.ConfigCmd == 0)
+			{
+				VS.ConfigCmd = 6;
+				if (IsDlgButtonChecked(hDlg,IDC_VS_DIP5) == BST_UNCHECKED)
+					VS.ConfigCmd += 8;
+				BlockDialog(hDlg,IDC_VS_DIP5);
+			}
+			else
+			{
+				UnblockDialog(hDlg);
+				VS.ConfigCmd = 0;
+			}
+			return TRUE;		break;
+		case IDC_VS_DIP6:
+			if (VS.ConfigCmd == 0)
+			{
+				VS.ConfigCmd = 7;
+				if (IsDlgButtonChecked(hDlg,IDC_VS_DIP6) == BST_UNCHECKED)
+					VS.ConfigCmd += 8;
+				BlockDialog(hDlg,IDC_VS_DIP6);
+			}
+			else
+			{
+				UnblockDialog(hDlg);
+				VS.ConfigCmd = 0;
+			}
+			return TRUE;		break;
+		case IDC_VS_DIP7:
+			if (VS.ConfigCmd == 0)
+			{
+				VS.ConfigCmd = 8;
+				if (IsDlgButtonChecked(hDlg,IDC_VS_DIP7) == BST_UNCHECKED)
+					VS.ConfigCmd += 8;
+				BlockDialog(hDlg,IDC_VS_DIP7);
+			}
+			else
+			{
+				UnblockDialog(hDlg);
+				VS.ConfigCmd = 0;
+			}
+			return TRUE;		break;
+		case IDC_VS_COIN1:
+			VS.ConfigCmd = 17;
+			BlockDialog(hDlg, 0);
+			return TRUE;		break;
+		case IDC_VS_COIN2:
+			VS.ConfigCmd = 18;
+			BlockDialog(hDlg, 0);
+			return TRUE;		break;
+		case IDC_VS_COIN3:
+			VS.ConfigCmd = 19;
+			BlockDialog(hDlg, 0);
+			return TRUE;		break;
+		case IDCLOSE:
 			VS.ConfigWindow = NULL;
 			DestroyWindow(hDlg);
 			return TRUE;		break;
+		}
+		break;
+	case WM_CLOSE:
+		VS.ConfigWindow = NULL;
+		DestroyWindow(hDlg);
+		return TRUE;		break;
 	}
 	return FALSE;
 }
@@ -256,24 +278,22 @@ unsigned char	_MAPINT	VS_Config (CFG_TYPE mode, unsigned char data)
 	case CFG_CMD:
 		switch (data)
 		{
-		case 0x01:	case 0x02:	case 0x03:	case 0x04:
-		case 0x05:	case 0x06:	case 0x07:	case 0x08:
+		case 1: case 2: case 3: case 4: case 5: case 6: case 7: case 8:
 			// set switch
 			VS.DipSwitch |= 1 << (data - 1);
 			UnblockDialog(VS.ConfigWindow);
 			break;
-		case 0x09:	case 0x0A:	case 0x0B:	case 0x0C:
-		case 0x0D:	case 0x0E:	case 0x0F:	case 0x10:
+		case 9: case 10: case 11: case 12: case 13: case 14: case 15: case 16:
 			// clear switch
 			VS.DipSwitch &= ~(1 << (data - 9));
 			UnblockDialog(VS.ConfigWindow);
 			break;
-		case 0x11:	case 0x12:	case 0x13:
-			if (data == 0x11)
+		case 17: case 18: case 19:
+			if (data == 17)
 				VS.Coin |= 0x20;
-			if (data == 0x12)
+			if (data == 18)
 				VS.Coin |= 0x40;
-			if (data == 0x13)
+			if (data == 19)
 				VS.Coin |= 0x04;
 			VS.CoinDelay = 444444;
 			UnblockDialog(VS.ConfigWindow);
