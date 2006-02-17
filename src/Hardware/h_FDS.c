530e965f3b937b846e22bf27f798389a3ef0ee92
Quietust <quietust@gmail.com>
2006-02-17 23:07:35 +0000

    Adjust IRQ delays to speed up disk loading

--- src/Hardware/h_FDS.c
+++ src/Hardware/h_FDS.c
@@ -57,8 +57,8 @@ void	_MAPINT	FDS_CPUCycle (void)
 		StartIRQ(IRQ_DISK);
 }
 
-#define	DISKIRQ_SHORT	150
-#define	DISKIRQ_LONG	200
+#define	DISKIRQ_SHORT	64
+#define	DISKIRQ_LONG	256
 
 int	_MAPINT	FDS_Read (int Bank, int Addr)
 {
