ef4c3d05f63702b9e9ea9c515221d06c8b21c1e8
Quietust <quietust@gmail.com>
2006-02-17 23:03:50 +0000

    Update VRC7 IRQ emulation

--- src/iNES/mapper085.c
+++ src/iNES/mapper085.c
@@ -3,8 +3,8 @@
 
 static	struct
 {
-	u8 IRQenabled, IRQtoggle, IRQlatch, IRQcounter;
-	u8 PRG[3], CHR[8], Mirror;
+	u8 IRQenabled, IRQlatch, IRQcounter;
+	u8 PRG[3], CHR[8], Misc;
 }	Mapper;
 
 static	void	Sync (void)
@@ -14,12 +14,19 @@ static	void	Sync (void)
 	EMU->SetPRG_ROM8(0xA,Mapper.PRG[1]);
 	EMU->SetPRG_ROM8(0xC,Mapper.PRG[2]);
 	EMU->SetPRG_ROM8(0xE,-1);
+	if (Mapper.Misc & 0x80)
+		EMU->SetPRG_RAM8(0x6,0);
+	else
+	{
+		EMU->SetPRG_OB4(0x6);
+		EMU->SetPRG_OB4(0x7);
+	}
 	if (ROM->INES_CHRSize)
 		for (x = 0; x < 8; x++)
 			EMU->SetCHR_ROM1(x,Mapper.CHR[x]);
 	else	for (x = 0; x < 8; x++)
 			EMU->SetCHR_RAM1(x,Mapper.CHR[x] & 7);
-	switch (Mapper.Mirror & 0x3)
+	switch (Mapper.Misc & 0x3)
 	{
 	case 0:	EMU->Mirror_V();	break;
 	case 1:	EMU->Mirror_H();	break;
@@ -32,27 +39,27 @@ static	int	_MAPINT	SaveLoad (int mode, int x, char *data)
 {
 	u8 i;
 	SAVELOAD_BYTE(mode,x,data,Mapper.IRQenabled)
-	SAVELOAD_BYTE(mode,x,data,Mapper.IRQtoggle)
-	SAVELOAD_BYTE(mode,x,data,Mapper.IRQlatch)
 	SAVELOAD_BYTE(mode,x,data,Mapper.IRQcounter)
+	SAVELOAD_BYTE(mode,x,data,Mapper.IRQlatch)
 	for (i = 0; i < 3; i++)
 		SAVELOAD_BYTE(mode,x,data,Mapper.PRG[i])
 	for (i = 0; i < 8; i++)
 		SAVELOAD_BYTE(mode,x,data,Mapper.CHR[i])
-	SAVELOAD_BYTE(mode,x,data,Mapper.Mirror)
+	SAVELOAD_BYTE(mode,x,data,Mapper.Misc)
 	x = VRC7sound_SaveLoad(mode,x,data);
 	if (mode == STATE_LOAD)
 		Sync();
 	return x;
 }
 
-#define IRQ_CYCLES 114
+#define IRQ_CYCLES 341
 static int IRQcycles = IRQ_CYCLES;
 static	void	_MAPINT	CPUCycle (void)
 {
-	if ((Mapper.IRQenabled) && (!--IRQcycles))
+	if ((Mapper.IRQenabled & 2) && ((Mapper.IRQenabled & 4) || ((IRQcycles -= 3) < 0)))
 	{
-		IRQcycles = IRQ_CYCLES;
+		if (!(Mapper.IRQenabled & 4))
+			IRQcycles += IRQ_CYCLES;
 		if (Mapper.IRQcounter == 0xFF)
 		{
 			Mapper.IRQcounter = Mapper.IRQlatch;
@@ -119,7 +126,7 @@ static	void	_MAPINT	WriteE (int Bank, int Where, int What)
 		Mapper.IRQlatch = What;
 	else
 	{
-		Mapper.Mirror = What;
+		Mapper.Misc = What;
 		Sync();
 	}
 }
@@ -128,15 +135,17 @@ static	void	_MAPINT	WriteF (int Bank, int Where, int What)
 {
 	if (Where & 0x18)
 	{
-		Mapper.IRQenabled = Mapper.IRQtoggle;
-		IRQcycles = IRQ_CYCLES;
+		if (Mapper.IRQenabled & 1)
+			Mapper.IRQenabled |= 0x2;
 	}
 	else
 	{
-		Mapper.IRQtoggle = What & 0x1;
-		Mapper.IRQenabled = What & 0x2;
-		if (Mapper.IRQenabled)
+		Mapper.IRQenabled = What & 0x7;
+		if (Mapper.IRQenabled & 0x2)
+		{
 			Mapper.IRQcounter = Mapper.IRQlatch;
+			IRQcycles = IRQ_CYCLES;
+		}
 	}
 	EMU->SetIRQ(1);
 }
@@ -168,7 +177,7 @@ static	void	_MAPINT	Reset (int IsHardReset)
 	EMU->SetCPUWriteHandler(0xE,WriteE);
 	EMU->SetCPUWriteHandler(0xF,WriteF);
 
-	Mapper.IRQenabled = Mapper.IRQtoggle = Mapper.IRQlatch = Mapper.IRQcounter = 0;
+	Mapper.IRQenabled = Mapper.IRQlatch = Mapper.IRQcounter = 0;
 	Mapper.PRG[0] = 0x00;	Mapper.PRG[0] = 0x01;	Mapper.PRG[2] = 0xFE;
 	for (x = 0; x < 8; x++)	Mapper.CHR[x] = x;
 
