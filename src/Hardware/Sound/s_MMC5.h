ba7491b57a9a2a60791e10fa77022f813bac6782
Quietust <quietust@gmail.com>
2006-02-17 23:10:20 +0000

    Update NSF mapper to interface v3.6

--- src/Hardware/Sound/s_MMC5.h
+++ src/Hardware/Sound/s_MMC5.h
@@ -1,10 +1,12 @@
 #ifndef	S_MMC5_H
 #define	S_MMC5_H
 
-void	MMC5sound_Init (void);
-void	MMC5sound_Write (int,int);
-int	_MAPINT	MMC5sound_Get (int);
-int	_MAPINT	MMC5sound_SaveLoad (STATE_TYPE,int,unsigned char *);
-void	MMC5sound_Destroy (void);
+void		MMC5sound_Load		(void);
+void		MMC5sound_Reset		(RESET_TYPE);
+void		MMC5sound_Unload	(void);
+void		MMC5sound_Write		(int,int);
+int		MMC5sound_Read		(int);
+int	_MAPINT	MMC5sound_Get		(int);
+int	_MAPINT	MMC5sound_SaveLoad	(STATE_TYPE,int,unsigned char *);
 
 #endif	/* S_MMC5_H */
