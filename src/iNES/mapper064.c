a6ecf443df8a99e82bc9047f4ed8e476fe796dcd
Quietust <quietust@gmail.com>
2006-02-17 23:02:41 +0000

    Fix RAMBO-1 IRQs, sort of (needs timing fixes in PPU)

--- src/iNES/mapper064.c
+++ src/iNES/mapper064.c
@@ -2,7 +2,8 @@
 
 static	struct
 {
-	u8 IRQenabled, IRQcounter, IRQlatch, IRQmode;
+	u8 IRQenabled, IRQcounter, IRQlatch, IRQmode, IRQreload;
+	u16 IRQaddr;
 	u8 Cmd;
 	u8 PRG[3];
 	u8 CHR[8];
@@ -30,8 +31,13 @@ static	void	Sync (void)
 	}
 	EMU->SetPRG_ROM8(0xE,-1);
 
-	for (x = 0; x < 8; x++)
+	for (x = 4; x < 8; x++)
 		EMU->SetCHR_ROM1(SwCHR ^ x,Mapper.CHR[x]);
+	for (x = 0; x < 4; x++)
+		EMU->SetCHR_ROM2(SwCHR ^ x,Mapper.CHR[x] >> 1);
+	if (Mapper.Cmd & 0x20)
+		for (x = 0; x < 8; x++)
+			EMU->SetCHR_ROM1(SwCHR ^ x,Mapper.CHR[x]);
 }
 
 static	int	_MAPINT	SaveLoad (int mode, int x, char *data)
@@ -51,39 +57,39 @@ static	int	_MAPINT	SaveLoad (int mode, int x, char *data)
 	return x;
 }
 
+static	void	HBlank (void)
+{
+	if (!Mapper.IRQcounter || Mapper.IRQreload)
+	{
+		Mapper.IRQcounter = Mapper.IRQlatch;
+		if (Mapper.IRQreload)
+			Mapper.IRQreload--;
+	}
+	else if (!--Mapper.IRQcounter)
+	{
+		EMU->SetIRQ(0);
+		Mapper.IRQreload = 2;
+	}
+}
+
 static	void	_MAPINT	PPUCycle (int Addr, int Scanline, int Cycle, int IsRendering)
 {
-	if ((Mapper.IRQenabled) && (Mapper.IRQmode == 0) && (IsRendering) && (Cycle == 320))
-	{
-		Mapper.IRQcounter--;
-		if (!Mapper.IRQcounter)
-		{
-			Mapper.IRQcounter = Mapper.IRQlatch;
-			EMU->SetIRQ(0);
-		}
-	}
+	if (Addr & 0x2000)
+		return;
+	if ((Mapper.IRQenabled) && !(Mapper.IRQmode) && !(Mapper.IRQaddr & 0x1000) && (Addr & 0x1000))
+		HBlank();
+	Mapper.IRQaddr = Addr;
 }
 
 static int cycles = 4;
-
 static	void	_MAPINT	CPUCycle (void)
 {
-	if ((Mapper.IRQenabled) && (Mapper.IRQmode == 1))
+	if ((Mapper.IRQenabled) && (Mapper.IRQmode) && (!--cycles))
 	{
-		cycles--;
-		if (!cycles)
-		{
-			cycles = 4;
-			Mapper.IRQcounter--;
-			if (!Mapper.IRQcounter)
-			{
-				Mapper.IRQcounter = Mapper.IRQlatch;
-				EMU->SetIRQ(0);
-			}
-		}
+		cycles = 4;
+		HBlank();
 	}
 }
-
 
 static	void	_MAPINT	Write89 (int Bank, int Where, int What)
 {
@@ -91,15 +97,16 @@ static	void	_MAPINT	Write89 (int Bank, int Where, int What)
 		switch (Mapper.Cmd & 0xF)
 		{
 		case 0:	Mapper.CHR[0] = What;	break;
-		case 8:	Mapper.CHR[1] = What;	break;
 		case 1:	Mapper.CHR[2] = What;	break;
-		case 9:	Mapper.CHR[3] = What;	break;
 		case 2:	Mapper.CHR[4] = What;	break;
 		case 3:	Mapper.CHR[5] = What;	break;
 		case 4:	Mapper.CHR[6] = What;	break;
 		case 5:	Mapper.CHR[7] = What;	break;
 		case 6:	Mapper.PRG[0] = What;	break;
 		case 7:	Mapper.PRG[1] = What;	break;
+
+		case 8:	Mapper.CHR[1] = What;	break;
+		case 9:	Mapper.CHR[3] = What;	break;
 		case 15:Mapper.PRG[2] = What;	break;
 		}
 	else	Mapper.Cmd = What;
@@ -118,16 +125,17 @@ static	void	_MAPINT	WriteCD (int Bank, int Where, int What)
 {
 	if (Where & 1)
 	{
-		Mapper.IRQlatch = What;
 		Mapper.IRQmode = What & 1;
+		Mapper.IRQreload = 1;
 	}
-	else	Mapper.IRQcounter = Mapper.IRQlatch = What;
+	else	Mapper.IRQlatch = What;
 }
 
 static	void	_MAPINT	WriteEF (int Bank, int Where, int What)
 {
 	Mapper.IRQenabled = (Where & 1);
-	EMU->SetIRQ(1);
+	if (!Mapper.IRQenabled)
+		EMU->SetIRQ(1);
 }
 
 static	void	_MAPINT	Shutdown (void)
@@ -152,7 +160,7 @@ static	void	_MAPINT	Reset (int IsHardReset)
 
 	for (x = 0; x < 3; x++)	Mapper.PRG[x] = 0xFF;
 	for (x = 0; x < 8; x++)	Mapper.CHR[x] = x;
-	Mapper.IRQenabled = Mapper.IRQcounter = Mapper.IRQlatch = 0;
+	Mapper.IRQenabled = Mapper.IRQcounter = Mapper.IRQlatch = Mapper.IRQmode = Mapper.IRQreload = Mapper.IRQaddr = 0;
 	Mapper.Cmd = 0;
 	Mapper.Mirror = 0;
 
