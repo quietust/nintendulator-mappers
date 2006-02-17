442a79057fb6ef128c36238e0779d95f2d7041d0
Quietust <quietust@gmail.com>
2006-02-17 23:02:22 +0000

    Whoops, make these vars static

--- src/Hardware/h_MMC6.c
+++ src/Hardware/h_MMC6.c
@@ -174,7 +174,7 @@ void	_MAPINT	MMC6_CPUWriteEF (int Bank, int Where, int What)
 	if (!MMC6.IRQenabled)
 		EMU->SetIRQ(1);
 }
-int lastAddr = 0;
+static int lastAddr = 0;
 void	_MAPINT	MMC6_PPUCycle (int Addr, int Scanline, int Cycle, int IsRendering)
 {
 	if (Addr & 0x2000)
