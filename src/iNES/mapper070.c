cc3cce8b5151f73a0d57f0cfedd63198e801149b
Quietust <quietust@gmail.com>
2006-02-17 23:01:16 +0000

    Add mirroring toggle to mapper 70

--- src/iNES/mapper070.c
+++ src/iNES/mapper070.c
@@ -6,6 +6,9 @@ static	void	Sync (void)
 	EMU->SetPRG_ROM16(0x8,(Latch.Data & 0x70) >> 4);
 	EMU->SetPRG_ROM16(0xC,-1);
 	EMU->SetCHR_ROM8(0,Latch.Data & 0x0F);
+	if (Latch.Data & 0x80)
+		EMU->Mirror_S1();
+	else	EMU->Mirror_S0();
 }
 
 static	void	_MAPINT	Shutdown (void)
