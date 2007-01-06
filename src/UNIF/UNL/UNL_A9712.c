bd165d52f5d6113636bd70d6e50470f47ffa71b4
Quietust <quietust@gmail.com>
2007-01-06 05:16:01 +0000

    Add a hack so the game is actually playable (though not terribly high quality); a proper implementation will come later

--- src/UNIF/UNL/UNL_A9712.c
+++ src/UNIF/UNL/UNL_A9712.c
@@ -9,31 +9,44 @@ static	struct
 static	void	Sync (void)
 {
 	MMC3_SyncWRAM();
-	MMC3_SyncPRG(0x3F,0);
 	MMC3_SyncCHR_ROM(0xFF,0);
 	MMC3_SyncMirror();
-	if (Mapper.Reg0 & 0x40)
-	{
-		u8 BankNum = (Mapper.Reg0 & 0x05) | ((Mapper.Reg0 & 8) >> 2) | ((Mapper.Reg0 & 0x20) >> 2);
-		if (Mapper.Reg0 & 0x2)
-			EMU->SetPRG_ROM32(0x8,BankNum >> 1);
-		else
-		{
-			EMU->SetPRG_ROM16(0x8,BankNum);
-			EMU->SetPRG_ROM16(0xC,BankNum);
-		}
-	}
+	MMC3_SyncPRG(0x3F,0);
+	if (Mapper.Reg1 == 0xAB)
+		EMU->SetPRG_ROM8(0xE,0x07);
+	if (Mapper.Reg1 == 0xFF)
+		EMU->SetPRG_ROM8(0xE,0x09);
+}
+
+static	int	MAPINT	SaveLoad (STATE_TYPE mode, int x, unsigned char *data)
+{
+	x = MMC3_SaveLoad(mode,x,data);
+	SAVELOAD_BYTE(mode,x,data,Mapper.Reg0)
+	SAVELOAD_BYTE(mode,x,data,Mapper.Reg1)
+	return x;
+}
+
+static	int	MAPINT	Read5 (int Bank, int Addr)
+{
+	if (Addr & 0x80)
+		return -1;
+	else	return 0x9F;
 }
 
 static	void	MAPINT	Write5 (int Bank, int Addr, int Val)
 {
-	if (Addr & 0x800)
+	if (Addr & 0x80)
+		Mapper.Reg0 = Val;
+}
+
+static	void	MAPINT	Write89 (int Bank, int Addr, int Val)
+{
+	if ((Addr & 0x3) == 3)
 	{
-		if (Addr & 1)
-			Mapper.Reg1 = Val;
-		else	Mapper.Reg0 = Val;
+		Mapper.Reg1 = Val;
 		Sync();
 	}
+	else	MMC3_CPUWrite89(Bank, Addr, Val);
 }
 
 static	void	MAPINT	Load (void)
@@ -43,7 +56,10 @@ static	void	MAPINT	Load (void)
 static	void	MAPINT	Reset (RESET_TYPE ResetType)
 {
 	MMC3_Reset(ResetType);
+	EMU->SetCPUReadHandler(0x5,Read5);
 	EMU->SetCPUWriteHandler(0x5,Write5);
+	EMU->SetCPUWriteHandler(0x8,Write89);
+	EMU->SetCPUWriteHandler(0x9,Write89);
 	if (ResetType == RESET_HARD)
 	{
 		Mapper.Reg0 = 0;
@@ -66,7 +82,7 @@ CTMapperInfo	MapperInfo_UNL_A9712 =
 	Unload,
 	NULL,
 	MMC3_PPUCycle,
-	MMC3_SaveLoad,
+	SaveLoad,
 	NULL,
 	NULL
 };
