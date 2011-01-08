0899f087eaa49bf3f90e65f72f0fac45c4f5f348
Quietust <quietust@gmail.com>
2011-01-08 04:06:48 +0000

    Add support for SOROM and SXROM to iNES mapper 1, provided the ROM is NES 2.0

--- src/iNES/mapper001.cpp
+++ src/iNES/mapper001.cpp
@@ -19,7 +19,29 @@ void	Sync (void)
 	if (ROM->INES_CHRSize)
 		MMC1::SyncCHR_ROM(0x1F, 0);
 	else	MMC1::SyncCHR_RAM(0x01, 0);
-	MMC1::SyncWRAM();
+	if (ROM->INES_Version == 2)
+	{
+		if (MMC1::GetWRAMEnabled())
+		{
+			int bank = 0;
+			if (ROM->INES2_PRGRAM == 0x77)	// SOROM
+			{
+				// bank 1 is batteried while bank 0 is not, so we need to switch it around
+				bank = !(MMC1::GetCHRBankLo() & 0x08);
+			}
+			else if (ROM->INES2_PRGRAM == 0x90)	// SXROM
+				bank = (MMC1::GetCHRBankLo() & 0x0C) >> 2;
+			else //if (ROM->INES2_PRGRAM == 0x70)	// everything else
+				bank = 0;
+			EMU->SetPRG_RAM8(0x6, bank);
+		}
+		else
+		{
+			EMU->SetPRG_OB4(0x6);
+			EMU->SetPRG_OB4(0x7);
+		}
+	}
+	else	MMC1::SyncWRAM();
 }
 
 void	MAPINT	Load (void)
