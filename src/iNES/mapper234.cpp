208c985d145edd5ba565c334c34b3f8fcbd2ff12
Quietust <quietust@gmail.com>
2012-07-12 01:51:44 +0000

    Minor cleanup and fix

--- src/iNES/mapper234.cpp
+++ src/iNES/mapper234.cpp
@@ -43,27 +43,35 @@ void	MAPINT	Write (int Bank, int Addr, int Val)
 {
 	switch (Addr & 0xFF8)
 	{
-	case 0xF80:
-	case 0xF88:
-	case 0xF90:
-	case 0xF98:	if (Reg1)	break;
-				Reg1 = Val;
-			Sync();			break;
-	case 0xFA0:
-	case 0xFA8:
-	case 0xFB0:
-	case 0xFB8:	break;
-	case 0xFC0:
-	case 0xFC8:
-	case 0xFD0:
-	case 0xFD8:	if (Reg3)	break;
-				Reg3 = Val;
-			Sync();			break;
-	case 0xFE0:	break;
-	case 0xFE8:
-	case 0xFF0:	Reg2 = Val;
-			Sync();			break;
-	case 0xFF8:	break;
+	case 0xF80:	case 0xF88:	case 0xF90:	case 0xF98:
+		if (Reg1 & 0x3F)
+			break;
+		Reg1 = Val;
+		Sync();
+		break;
+
+	case 0xFA0:	case 0xFA8:	case 0xFB0:	case 0xFB8:
+		// nothing here
+		break;
+
+	case 0xFC0:	case 0xFC8:	case 0xFD0:	case 0xFD8:
+		if (Reg1 & 0x3F)
+			break;
+		// just lockout defeat, but we'll store it anyways
+		Reg3 = Val & 0x3;
+		Sync();
+		break;
+
+	case 0xFE0:
+		break;
+
+	case 0xFE8:	case 0xFF0:
+		Reg2 = Val;
+		Sync();
+		break;
+
+	case 0xFF8:
+		break;
 	}
 }
 
