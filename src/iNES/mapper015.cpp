0ae1e4b6f174b400a1635a5e0080e6b221f9fc08
Quietust <quietust@gmail.com>
2020-05-10 08:51:12 -0600

    Update mapper 15 to match actual hardware
    * Data bit 0x80 was completely wrong (only affects mode 2)
    * Mode 1 only worked correctly with banks 0x38-0x3F
    * Savestate data simplified

--- src/iNES/mapper015.cpp
+++ src/iNES/mapper015.cpp
@@ -6,28 +6,69 @@
 
 namespace
 {
-uint8_t PRG[4];
-uint8_t Mirror;
+uint8_t BankReg, Mode;
 
 void	Sync (void)
 {
-	EMU->SetPRG_RAM8(0x6, 0);
-	for (int i = 0; i < 4; i++)
-		EMU->SetPRG_ROM8(0x8 | (i << 1), PRG[i]);
-	EMU->SetCHR_RAM8(0, 0);
-	if (Mirror)
+	EMU->SetPRG_RAM8(0x6, 0); // For compatibility with mapper hacks
+	switch (Mode)
+	{
+	case 0:
+		EMU->SetPRG_ROM32(0x8, (BankReg & 0x3E) >> 1);
+		break;
+	case 1:
+		EMU->SetPRG_ROM16(0x8, (BankReg & 0x3F));
+		EMU->SetPRG_ROM16(0xC, (BankReg & 0x38) | 0x07);
+		break;
+	case 2:
+		EMU->SetPRG_ROM8(0x8, ((BankReg & 0x3F) << 1) | ((BankReg & 0x80) >> 7));
+		EMU->SetPRG_ROM8(0xA, ((BankReg & 0x3F) << 1) | ((BankReg & 0x80) >> 7));
+		EMU->SetPRG_ROM8(0xC, ((BankReg & 0x3F) << 1) | ((BankReg & 0x80) >> 7));
+		EMU->SetPRG_ROM8(0xE, ((BankReg & 0x3F) << 1) | ((BankReg & 0x80) >> 7));
+		break;
+	case 3:
+		EMU->SetPRG_ROM16(0x8, (BankReg & 0x3F));
+		EMU->SetPRG_ROM16(0xC, (BankReg & 0x3F));
+		break;
+	}
+	EMU->SetCHR_RAM8(0, 0);	// For compatibility with mapper hacks, do not write-protect
+	if (BankReg & 0x40)
 		EMU->Mirror_H();
 	else	EMU->Mirror_V();
 }
 
 int	MAPINT	SaveLoad (STATE_TYPE mode, int offset, unsigned char *data)
 {
-	uint8_t ver = 0;
+	uint8_t ver = 1;
 	CheckSave(SAVELOAD_VERSION(mode, offset, data, ver));
 
-	for (int i = 0; i < 4; i++)
-		SAVELOAD_BYTE(mode, offset, data, PRG[i]);
-	SAVELOAD_BYTE(mode, offset, data, Mirror);
+	// Translate old savestate data
+	if (ver == 0)
+	{
+		uint8_t PRG[4];
+		uint8_t Mirror;
+
+		for (int i = 0; i < 4; i++)
+			SAVELOAD_BYTE(mode, offset, data, PRG[i]);
+		SAVELOAD_BYTE(mode, offset, data, Mirror);
+
+		BankReg = ((PRG[0] >> 1) & 0x3F) | Mirror;
+		if (PRG[0] == PRG[1])
+		{
+			Mode = 2;
+			BankReg |= (PRG[0] & 1) << 7;
+		}
+		else if (PRG[0] == PRG[2])
+			Mode = 3;
+		else if (PRG[2] == (PRG[0] | 2))
+			Mode = 0;
+		else	Mode = 1;
+	}
+	else
+	{
+		SAVELOAD_BYTE(mode, offset, data, BankReg);
+		SAVELOAD_BYTE(mode, offset, data, Mode);
+	}
 
 	if (IsLoad(mode))
 		Sync();
@@ -36,37 +77,8 @@ int	MAPINT	SaveLoad (STATE_TYPE mode, int offset, unsigned char *data)
 
 void	MAPINT	Write (int Bank, int Addr, int Val)
 {
-	uint8_t PRGbank = (Val & 0x3F) << 1;
-	uint8_t PRGflip = (Val & 0x80) >> 7;
-	Mirror = Val & 0x40;
-	switch (Addr)
-	{
-	case 0x000:
-		PRGbank &= 0x7C;
-		PRG[0] = PRGbank | 0 ^ PRGflip;
-		PRG[1] = PRGbank | 1 ^ PRGflip;
-		PRG[2] = PRGbank | 2 ^ PRGflip;
-		PRG[3] = PRGbank | 3 ^ PRGflip;
-		break;
-	case 0x001:
-		PRG[0] = PRGbank | 0 ^ PRGflip;
-		PRG[1] = PRGbank | 1 ^ PRGflip;
-		PRG[2] = 0x7E | 0 ^ PRGflip;
-		PRG[3] = 0x7F | 1 ^ PRGflip;
-		break;
-	case 0x002:
-		PRG[0] = PRGbank ^ PRGflip;
-		PRG[1] = PRGbank ^ PRGflip;
-		PRG[2] = PRGbank ^ PRGflip;
-		PRG[3] = PRGbank ^ PRGflip;
-		break;
-	case 0x003:
-		PRG[0] = PRGbank | 0 ^ PRGflip;
-		PRG[1] = PRGbank | 1 ^ PRGflip;
-		PRG[2] = PRGbank | 0 ^ PRGflip;
-		PRG[3] = PRGbank | 1 ^ PRGflip;
-		break;
-	}
+	Mode = (Addr & 0x3);
+	BankReg = Val;
 	Sync();
 }
 
@@ -77,14 +89,11 @@ BOOL	MAPINT	Load (void)
 }
 void	MAPINT	Reset (RESET_TYPE ResetType)
 {
-	EMU->SetCPUWriteHandler(0x8, Write);
+	for (int i = 0x8; i < 0x10; i++)
+		EMU->SetCPUWriteHandler(i, Write);
 
 	if (ResetType == RESET_HARD)
-	{
-		for (int i = 0; i < 4; i++)
-			PRG[i] = i;
-		Mirror = 0;
-	}
+		BankReg = Mode = 0;
 	Sync();
 }
 
@@ -94,7 +103,7 @@ uint16_t MapperNum = 15;
 const MapperInfo MapperInfo_015 =
 {
 	&MapperNum,
-	_T("100-in-1 Contra Function 16"),
+	_T("K-1029 (100-in-1 Contra Function 16)"),
 	COMPAT_FULL,
 	Load,
 	Reset,
