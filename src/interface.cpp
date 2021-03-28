9c9de797ed7638ab8b185414f89a6e30829d55ac
Quietust <quietust@gmail.com>
2021-03-27 19:03:29 -0600

    Move SAVELOAD functions into interface.cpp, no need to inline them anymore

--- src/interface.cpp
+++ src/interface.cpp
@@ -95,3 +95,140 @@ const MapperInfo *findByName (const char *BoardName)
 	}
 	return NULL;
 }
+
+int SAVELOAD_VERSION(STATE_TYPE mode, int &offset, unsigned char *data, uint8_t &cur_ver)
+{
+	switch (mode)
+	{
+	case STATE_SAVE:
+		data[offset++] = cur_ver;
+		break;
+	case STATE_LOAD_VER:
+		if (data[offset] > cur_ver)
+			return -1;
+		cur_ver = data[offset++];
+		break;
+	case STATE_LOAD:
+		cur_ver = 0;
+		break;
+	case STATE_SIZE:
+		offset++;
+		break;
+	default:
+		MessageBox(hWnd, _T("Invalid save/load type!"), _T("Mapper DLL"), MB_OK);
+	}
+	return 0;
+}
+
+void SAVELOAD_BYTE(STATE_TYPE mode, int &offset, unsigned char *data, uint8_t &value)
+{
+	switch (mode)
+	{
+	case STATE_SAVE:
+		data[offset++] = value;
+		break;
+	case STATE_LOAD_VER:
+	case STATE_LOAD:
+		value = data[offset++];
+		break;
+	case STATE_SIZE:
+		offset++;
+		break;
+	default:
+		MessageBox(hWnd, _T("Invalid save/load type!"), _T("Mapper DLL"), MB_OK);
+	}
+}
+void SAVELOAD_BYTE(STATE_TYPE mode, int &offset, unsigned char *data, int8_t &value)
+{
+	uint8_t _value = value;
+	SAVELOAD_BYTE(mode, offset, data, _value);
+	value = _value;
+}
+void SAVELOAD_BYTE(STATE_TYPE mode, int &offset, unsigned char *data, int &value)
+{
+	uint8_t _value = value;
+	SAVELOAD_BYTE(mode, offset, data, _value);
+	value = _value;
+}
+
+void SAVELOAD_WORD(STATE_TYPE mode, int &offset, unsigned char *data, uint16_t &value)
+{
+	uint16_n sl_tmp;
+	switch (mode)
+	{
+	case STATE_SAVE:
+		sl_tmp.s0 = value;
+		data[offset++] = sl_tmp.b0;
+		data[offset++] = sl_tmp.b1;
+		break;
+	case STATE_LOAD_VER:
+	case STATE_LOAD:
+		sl_tmp.b0 = data[offset++];
+		sl_tmp.b1 = data[offset++];
+		value = sl_tmp.s0;
+		break;
+	case STATE_SIZE:
+		offset += 2;
+		break;
+	default:
+		MessageBox(hWnd, _T("Invalid save/load type!"), _T("Mapper DLL"), MB_OK);
+	}
+}
+void SAVELOAD_WORD(STATE_TYPE mode, int &offset, unsigned char *data, int16_t &value)
+{
+	uint16_t _value = value;
+	SAVELOAD_WORD(mode, offset, data, _value);
+	value = _value;
+}
+void SAVELOAD_WORD(STATE_TYPE mode, int &offset, unsigned char *data, int &value)
+{
+	uint16_t _value = value;
+	SAVELOAD_WORD(mode, offset, data, _value);
+	value = _value;
+}
+
+void SAVELOAD_LONG(STATE_TYPE mode, int &offset, unsigned char *data, uint32_t &value)
+{
+	uint32_n sl_tmp;
+	switch (mode)
+	{
+	case STATE_SAVE:
+		sl_tmp.l0 = value;
+		data[offset++] = sl_tmp.b0;
+		data[offset++] = sl_tmp.b1;
+		data[offset++] = sl_tmp.b2;
+		data[offset++] = sl_tmp.b3;
+		break;
+	case STATE_LOAD_VER:
+	case STATE_LOAD:
+		sl_tmp.b0 = data[offset++];
+		sl_tmp.b1 = data[offset++];
+		sl_tmp.b2 = data[offset++];
+		sl_tmp.b3 = data[offset++];
+		value = sl_tmp.l0;
+		break;
+	case STATE_SIZE:
+		offset += 4;
+		break;
+	default:
+		MessageBox(hWnd, _T("Invalid save/load type!"), _T("Mapper DLL"), MB_OK);
+	}
+}
+void SAVELOAD_LONG(STATE_TYPE mode, int &offset, unsigned char *data, int32_t &value)
+{
+	uint32_t _value = value;
+	SAVELOAD_LONG(mode, offset, data, _value);
+	value = _value;
+}
+void SAVELOAD_LONG(STATE_TYPE mode, int &offset, unsigned char *data, long &value)
+{
+	uint32_t _value = value;
+	SAVELOAD_LONG(mode, offset, data, _value);
+	value = _value;
+}
+void SAVELOAD_LONG(STATE_TYPE mode, int &offset, unsigned char *data, unsigned long &value)
+{
+	uint32_t _value = value;
+	SAVELOAD_LONG(mode, offset, data, _value);
+	value = _value;
+}
