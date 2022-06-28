8d16a6319a86b74347e1402dd16689fd942bae8b
Quietust <quietust@gmail.com>
2022-06-28 14:18:59 -0600

    Update mapper 78 to support NES 2.0 submappers
    and retain 4-screen VRAM flag fallback

--- src/iNES/mapper078.cpp
+++ src/iNES/mapper078.cpp
@@ -7,18 +7,40 @@
 
 namespace
 {
+enum MIRROR	{ MIRROR_S0S1, MIRROR_HV };
+MIRROR MirrMode;
+
 void	Sync (void)
 {
 	EMU->SetPRG_ROM16(0x8, Latch::Data & 0x7);
 	EMU->SetPRG_ROM16(0xC, -1);
 	EMU->SetCHR_ROM8(0x0, (Latch::Data & 0xF0) >> 4);
-	if (Latch::Data & 0x8)
-		((ROM->INES_Flags & 0x08) ? EMU->Mirror_V : EMU->Mirror_S1)();
-	else	((ROM->INES_Flags & 0x08) ? EMU->Mirror_H : EMU->Mirror_S0)();
+	switch (MirrMode)
+	{
+	case MIRROR_S0S1:
+		if (Latch::Data & 0x8)
+			EMU->Mirror_S1();
+		else	EMU->Mirror_S0();
+		break;
+	case MIRROR_HV:
+		if (Latch::Data & 0x8)
+			EMU->Mirror_V();
+		else	EMU->Mirror_H();
+		break;
+	}
 }
 
 BOOL	MAPINT	Load (void)
 {
+	MirrMode = (ROM->INES_Flags & 0x08) ? MIRROR_HV : MIRROR_S0S1;
+	if (ROM->INES_Version == 2)
+	{
+		switch (ROM->INES2_SubMapper)
+		{
+		case 1:	MirrMode = MIRROR_S0S1;	break;
+		case 3:	MirrMode = MIRROR_HV;	break;
+		}
+	}
 	Latch::Load(Sync, FALSE, TRUE);
 	return TRUE;
 }
