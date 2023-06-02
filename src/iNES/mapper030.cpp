4e03e33b57ac42579496cf8ef73afc9378ca69f4
Quietust <quietust@gmail.com>
2023-06-02 12:28:07 -0600

    Add basic support for mapper 30 (UNROM 512)
    Currently does NOT support flashable games.

---
+++ src/iNES/mapper030.cpp
@@ -0,0 +1,73 @@
+/* Nintendulator Mapper DLLs
+ * Copyright (C) QMT Productions
+ */
+
+#include	"..\DLL\d_iNES.h"
+#include	"..\Hardware\h_Latch.h"
+
+namespace
+{
+bool WroteFlash;
+void	Sync (void)
+{
+	EMU->SetPRG_ROM16(0x8, Latch::Data & 0x1F);
+	EMU->SetPRG_ROM16(0xC, 0x1F);
+	EMU->SetCHR_RAM8(0x0, (Latch::Data & 0x60) >> 5);
+	if ((ROM->INES_Flags & 0x09) == 0x08)
+	{
+		if (Latch::Data & 0x80)
+			EMU->Mirror_S1();
+		else	EMU->Mirror_S0();
+	}
+}
+
+void	MAPINT	WriteFlash (int Bank, int Addr, int Val)
+{
+	if (!WroteFlash)
+		EMU->DbgOut(_T("ERROR: game attempted to write to Flash ROM!"));
+	WroteFlash = true;
+}
+
+BOOL	MAPINT	Load (void)
+{
+	if (ROM->INES_Flags & 0x02)
+		Latch::Load(Sync, FALSE, TRUE);
+	else
+		Latch::Load(Sync, TRUE, TRUE);
+	return TRUE;
+}
+void	MAPINT	Reset (RESET_TYPE ResetType)
+{
+	WroteFlash = false;
+	iNES_SetMirroring();
+	Latch::Reset(ResetType);
+	if (ROM->INES_Flags & 0x02)
+	{
+		EMU->SetCPUWriteHandler(0x8, WriteFlash);
+		EMU->SetCPUWriteHandler(0x9, WriteFlash);
+		EMU->SetCPUWriteHandler(0xA, WriteFlash);
+		EMU->SetCPUWriteHandler(0xB, WriteFlash);
+	}
+}
+void	MAPINT	Unload (void)
+{
+	Latch::Unload();
+}
+
+uint16_t MapperNum = 30;
+} // namespace
+
+const MapperInfo MapperInfo_002
+(
+	&MapperNum,
+	_T("UNROM 512"),
+	COMPAT_NEARLY,
+	Load,
+	Reset,
+	Unload,
+	NULL,
+	NULL,
+	Latch::SaveLoad_D,
+	NULL,
+	NULL
+);
